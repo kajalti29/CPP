f5777b96c8900daac431c4df3a0894d74fc5c022
kajalti29 <kajaltisirame@gmail.com>
2025-09-08 17:52:06 +0530

    new commit

--- 1_C++/1_Basic/1_C++/11_Loops/1_loop.cpp
+++ 1_C++/1_Basic/1_C++/11_Loops/1_loop.cpp
@@ -3,3 +3,29 @@
 // hile and do while loop
 // Questions using operators
 // Pattern printing problems
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     // int n;
+//     // cout<<"Enter n : ";
+//     // cin>>n;
+//     for(int i=1; i<=10; i++){
+//         cout<<"Hello World"<<endl;
+//     }
+// return 0;
+// }
+
+
+// Print number from 1 to 100
+
+#include<iostream>
+using namespace std;
+int main(){
+    for( int i=1; i<=100; i++){
+        cout<<i<<endl;
+    }
+return 0;
+}
