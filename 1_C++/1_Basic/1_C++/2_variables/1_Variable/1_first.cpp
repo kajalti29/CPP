829698231193358f1339040b28975cb590492983
kajalti29 <kajaltisirame@gmail.com>
2025-09-11 18:32:00 +0530

    new commit

---
+++ 1_C++/1_Basic/1_C++/2_variables/1_Variable/1_first.cpp
@@ -0,0 +1,18 @@
+// Variable: A name for a memory location.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;       // integer variable
+    float price = 99.5; // decimal value
+    char grade = 'A';   // single character
+    bool isPass = true; // true/false
+
+    cout << "Age = " << age << endl;
+    cout << "Price = " << price << endl;
+    cout << "Grade = " << grade << endl;
+    cout << "Pass = " << isPass << endl; // true = 1, false = 0
+    return 0;
+}
