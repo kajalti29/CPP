306ccfff99d77da13a2f10a88345eacabdd8dc47
kajalti29 <kajaltisirame@gmail.com>
2025-09-11 18:40:52 +0530

    new commit

---
+++ 1_C++/1_Basic/1_C++/2_variables/2_DATA TYPES/1_first.cpp
@@ -0,0 +1,32 @@
+// Data Types:
+
+// int → integers (10, -20)
+
+// float → decimal numbers (3.14, -2.5)
+
+// double → large floating-point numbers
+
+// char → single character ('A', 'b', '3')
+
+// bool → true/false
+
+// string → text ("Hello")
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int roll = 23;
+    float price = 45.3;
+    char grade = 'A';
+    bool passed = true;
+    string name = "kajalati";
+
+    cout<<"roll:"<<roll<<endl;
+    cout<<"price:"<<price<<endl;
+    cout<<"grade:"<<grade<<endl;
+    cout<<"passed:"<<passed<<endl;
+    cout<<"name:"<<name<<endl;
+}
