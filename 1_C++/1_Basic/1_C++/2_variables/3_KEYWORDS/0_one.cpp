6ecd8082359843d7a3c95094a093638bf6422c57
kajalti29 <kajaltisirame@gmail.com>
2025-09-11 18:44:18 +0530

    new change

---
+++ 1_C++/1_Basic/1_C++/2_variables/3_KEYWORDS/0_one.cpp
@@ -0,0 +1,20 @@
+// . Keywords & Identifiers
+
+// Keywords → Reserved words (cannot be used as variable names).
+// Examples: int, float, if, else, while, return, class, for, switch, break
+
+// Identifiers → Names given to variables, functions, etc.
+
+// Rules:
+
+// Must start with letter or _
+
+// Cannot use spaces or special symbols
+
+// Case-sensitive (Age ≠ age)
+
+// 👉 Example:
+
+int number = 10;    // valid
+float marks_1 = 99; // valid
+// int for = 5;    ❌ invalid (keyword)
