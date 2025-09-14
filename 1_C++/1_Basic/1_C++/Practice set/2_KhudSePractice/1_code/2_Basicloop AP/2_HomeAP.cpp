91165bb1f11ca983de59e714a7b2b7d84de965ab
kajalti29 <kajaltisirame@gmail.com>
2025-09-14 09:02:05 +0530

    ao commit

---
+++ 1_C++/1_Basic/1_C++/Practice set/2_KhudSePractice/1_code/2_Basicloop AP/2_HomeAP.cpp
@@ -0,0 +1,36 @@
+// // Display this AP - 4, 7, 10, 13, 16..upto 'n'terms
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter n :";
+//     cin>>n;
+
+//     for(int i =4; i<=3*n+1; i+=3){
+//         cout<<i<<" ";   
+//     }
+// }
+
+
+ 
+                // OR
+// Arithmetic progesstion
+// Display this AP - 4,7,10,13,16,...upto '
+// 'n'terms
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+        int a =4;
+    for(int i=1;i<=n;i++){
+        cout<<a<<" ";
+        a+=3;// a=a+3
+
+    }
+}
+
+
+
+
---
+++ 1_C++/1_Basic/1_C++/Practice set/2_KhudSePractice/1_code/2_Basicloop AP/3_GP.cpp
@@ -0,0 +1,18 @@
+// disply this GP - 1,2,4,8,16,32,...upto 'n'terms
+
+
+//Number se multiply mtlb GP
+#include <iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+        int a =1;
+    for(int i=1;i<=n;i++){
+        cout<<a<<" ";
+        a *=2;// a=a*2
+
+    }
+}
