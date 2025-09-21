6e628840b3074c9b3df918d9d00aef87bc910ddf
kajalti29 <kajaltisirame@gmail.com>
2025-09-21 17:08:47 +0530

    changes commit

--- 1_C++/1_Basic/1_C++/21_FUNCTIONS/7_LibraryFun.cpp
+++ 1_C++/1_Basic/1_C++/21_FUNCTIONS/7_LibraryFun.cpp
@@ -1,12 +1,24 @@
-#include<iostream>
-#include<cmath>
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+
+// int main() {
+//     cout << sqrt(25) << endl;   // 5 (square root)
+//     cout << pow(2,3) << endl;   // 8 (2^3)
+//     cout << abs(-10) << endl;   // 10 (absolute value)
+//     cout << ceil(3.4) << endl;  // 4 (round up)
+//     cout << floor(3.9) << endl; // 3 (round down)
+//     return 0;
+// }
+
+
+// <iostream> (Input/Output)
+#include <iostream>
 using namespace std;
 
-int main() {
-    cout << sqrt(25) << endl;   // 5 (square root)
-    cout << pow(2,3) << endl;   // 8 (2^3)
-    cout << abs(-10) << endl;   // 10 (absolute value)
-    cout << ceil(3.4) << endl;  // 4 (round up)
-    cout << floor(3.9) << endl; // 3 (round down)
+int main(){
+    cout << "Hello World" << endl;   // output
+    int x;
+    cin >> x;   // input
     return 0;
 }
---
+++ 1_C++/1_Basic/1_C++/21_FUNCTIONS/8_Librarystr.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name = "Kajal";
+    cout << name.length() << endl;   // 5
+    cout << name.substr(0,3) << endl; // Kaj
+    return 0;
+}
