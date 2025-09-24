6169e8a821a588adb77c6ab5be4fe9039ffa13d6
kajalti29 <kajaltisirame@gmail.com>
2025-09-25 00:15:09 +0530

    change commit

---
+++ 1_C++/1_Basic/1_C++/25_Recursion/11_practice.cpp
@@ -0,0 +1,17 @@
+// Ques:- Make a function which calculates the factorial of n using recursion.
+
+#include<iostream>
+using namespace std;
+int fact(int n){
+    if(n == 0 || n ==1) return 1;
+    return n * fact(n - 1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    cout<<fact(n);
+    
+return 0;
+}
