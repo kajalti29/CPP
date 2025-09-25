7cec3897a962dfbd5d97af42ccb3ad4159657b3d
kajalti29 <kajaltisirame@gmail.com>
2025-09-25 18:36:45 +0530

    array commit

---
+++ 1_C++/1_Basic/1_C++/26_ARRAYS/1_array.cpp
@@ -0,0 +1,17 @@
+// Array:_
+// Array एक collection of elements है, जिसमें एक ही type (int, float, char आदि) के multiple values को एक ही नाम से store किया जाता है।
+
+// data_type array_name[size];
+
+int numbers[5];   
+// यह 5 integers का array है
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5];
+return 0;
+}
