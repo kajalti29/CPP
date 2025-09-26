f7d8c96fadbeef372cbe636a2d002e8f5dd675e6
kajalti29 <kajaltisirame@gmail.com>
2025-09-26 14:15:46 +0530

    class

---
+++ 1_C++/1_Basic/2_OOPS/1_notes.md
@@ -0,0 +1,20 @@
+Classes & OBjects:-
+
+
+Objects:-Objects are entities in the real world.
+Class:- class is like a blueprint of these entities.
+
+[A class is a user-defined data type in C++.
+It is like a blueprint that defines data (attributes) and functions (methods) together.]
+
+syntax:-
+class teacher {
+
+};
+
+<!-- Access Modifiers:-
+ 1. private:- data & methods accessible inside class
+
+2. Public:- Data & methods accessible to everyone.
+
+3. protected :- Data & method accessible inside class & to its derived class. -->
---
+++ 1_C++/1_Basic/2_OOPS/2_class.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Teacher{
+    public:
+    //properties
+    string name;
+    string dept;
+    string subject;
+    double salary;
+
+    //methods
+    void change(string newDept){
+        dept = newDept;
+    }
+};
+int main(){
+    Teacher t1; // obj
+    t1.name = "kajalti";
+    t1.subject = "c++";
+    t1.dept = "computer science";
+    t1.salary = 25000;
+
+    cout << "Name: " << t1.name << endl;
+    cout << "Department: " << t1.dept << endl;
+    cout << "Subject: " << t1.subject << endl;
+    cout << "Salary: " << t1.salary << endl;  
+return 0;
+}
