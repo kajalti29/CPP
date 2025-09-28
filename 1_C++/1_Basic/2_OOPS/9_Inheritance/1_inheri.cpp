691d209ce993f55d35b7fd7208403158d330a43b
kajalti29 <kajaltisirame@gmail.com>
2025-09-28 16:43:06 +0530

    code commit

---
+++ 1_C++/1_Basic/2_OOPS/9_Inheritance/1_inheri.cpp
@@ -0,0 +1,6 @@
+// INHERITANCE:- Inheritance एक OOP concept है जिसमें एक class (child/derived) दूसरी class (parent/base) के properties और methods को use कर सकती है।
+
+// Purpose:
+
+// Code reuse
+// Extend functionality without rewriting code
