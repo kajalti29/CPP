3a0be2900088024533974df31d150730ce98f8b5
kajalti29 <kajaltisirame@gmail.com>
2025-09-28 16:22:25 +0530

    change commit

--- 1_C++/1_Basic/2_OOPS/8_Destructor/1_one.cpp
+++ 1_C++/1_Basic/2_OOPS/8_Destructor/1_one.cpp
@@ -1,3 +1,7 @@
+
+//Desctructor:_ ye ek special member function hai jo object ka scope hone pr automatically callhota hai.
+//              iska kam hota hai dynamic memory, resources, file handles aadi ko clean karta hai.
+
 #include <iostream>
 using namespace std;
 
