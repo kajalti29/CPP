788f02a250d7f6379d2acc09fc5a8d4532649a3e
kajalti29 <kajaltisirame@gmail.com>
2025-09-30 14:37:54 +0530

    save commit

---
+++ 1_C++/1_Basic/3_STL/6_Pair/2_pair.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// #include<vector>
+// #include<list>
+// #include<deque>
+// using namespace std;
+
+
+// int main(){
+//     vector<pair<int, int>> vec = {{1, 2}, {2, 3},{3, 4}};
+//     // pair<string, int> p = {"kajalti",5};
+    
+//     for(auto p : vec){
+//         cout<< p.first << " " << p.second <<endl;
+//     }  
+// return 0;
+// };
