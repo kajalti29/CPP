9081ec9ea24c1deea954c7fed7ce2e5344315bf5
kajalti29 <kajaltisirame@gmail.com>
2025-09-30 15:19:11 +0530

    map commit

---
+++ 1_C++/1_Basic/3_STL/11_Map/2_insert.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// #include<vector>
+// #include<list>
+// #include<deque>
+// #include<queue>
+// #include<map>
+// using namespace std;
+
+// int main(){
+//     map<string, int> m;
+
+//     m["tv"] = 100;
+//     m["laptop"] = 100;
+//     m["headphones"] = 50;
+//     m["tablet"] = 120;
+//     m["watch"] = 50;
+
+
+//     m.emplace("camra", 25);
+//     for(auto p: m){
+//         cout<<p.first<<" "<<p.second<<endl;
+//     }
+
+//     cout<< "count = " <<m.count("laptop") <<endl;
+//     return 0;
+// };
