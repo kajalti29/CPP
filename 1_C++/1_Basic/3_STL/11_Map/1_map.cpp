681bf5e87838c0075280e439a03b39ce9e9cf4a4
kajalti29 <kajaltisirame@gmail.com>
2025-09-30 15:15:08 +0530

    code commit

---
+++ 1_C++/1_Basic/3_STL/11_Map/1_map.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// #include<vector>
+// #include<list>
+// #include<deque>
+// #include<queue>
+// #include<map>
+// using namespace std;
+
+// int main(){
+//     map<string, int> m;
+
+//     m["tv"] = 100;
+//     m["laptop"] = 100;
+//     m["headphones"] = 50;
+
+//     for(auto p: m){
+//         cout<<p.first<<" "<<p.second<<endl;
+//     }
+//     return 0;
+// };
---
+++ 1_C++/1_Basic/3_STL/9_Queue/1_one.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+// #include <queue>
+// using namespace std;
+
+// int main() {
+//     queue<int> q;
+
+//     // Push elements into queue
+//     q.push(1);
+//     q.push(2);
+//     q.push(3);
+
+//     // Print and pop elements
+//     cout << "Queue elements: ";
+//     while(!q.empty()) {
+//         cout << q.front() << " ";  // front element
+//         q.pop();                   // remove front element
+//     }
+//     cout << endl;
+
+//     return 0;
+// }
