b617da60475cac034d015f82a1c2eb896ff9c314
kajalti29 <kajaltisirame@gmail.com>
2025-09-30 14:51:40 +0530

    code commit

---
+++ 1_C++/1_Basic/3_STL/7_PushAndEmpface/1_one.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<pair<int, int>> vec = {{1, 2}, {2, 3}, {3, 4}};
+
+//     // Push_back and emplace_back
+//     vec.push_back({4, 5});       // correct syntax
+//     vec.emplace_back(5, 6);      // direct construct
+
+//     // Print all pairs
+//     for(auto p : vec) {
+//         cout << p.first << " " << p.second << endl;
+//     }
+
+//     return 0;
+// }
