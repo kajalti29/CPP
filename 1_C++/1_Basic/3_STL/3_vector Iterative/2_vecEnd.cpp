73546d7c1d177b3472c2c7321cdb7a8a8eebe140
kajalti29 <kajaltisirame@gmail.com>
2025-09-30 14:08:31 +0530

    change commit

--- 1_C++/1_Basic/3_STL/3_vector Iterative/2_vecEnd.cpp
+++ 1_C++/1_Basic/3_STL/3_vector Iterative/2_vecEnd.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(){
     vector<int> vec = {1, 2, 3, 4, 5};
 
-    vector<int>::iterator it;
+    vector<int>::reserve_iterator it;
 
-    for(it = vec.begin(); it != vec.end(); it++){
+    for(it = vec.rbegin(); it != vec.rend(); it++){
         cout << *(it) << " ";
     }
     
