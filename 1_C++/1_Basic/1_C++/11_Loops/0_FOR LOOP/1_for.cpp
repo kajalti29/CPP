af7573af3d4f03559eee3371a54dc90bb49dfb9c
kajalti29 <kajaltisirame@gmail.com>
2025-10-02 21:57:04 +0530

    change commit

--- 1_C++/1_Basic/1_C++/11_Loops/0_FOR LOOP/1_for.cpp
+++ 1_C++/1_Basic/1_C++/11_Loops/0_FOR LOOP/1_for.cpp
@@ -1,13 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    for(int i = 1; i <= 3; i++){
-    for(int j = 1; j <= 3; j++){
-        cout << i << "*" << j << "=" << i*j << "  ";
+int main() {
+    for(int i = 1; i <= 5; i++) {
+        cout << i << " ";
     }
-    cout << endl;
-}
-
-return 0;
+    return 0;
 }
