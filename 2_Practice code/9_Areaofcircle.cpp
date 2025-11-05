c4fb6616ec62109b4ce9f8cefbfa522954e9072f
kajalti@29 <kajaltisirame@gmail.com>
2025-11-05 14:13:12 +0530

    avg commit

---
+++ 2_Practice code/9_Areaofcircle.cpp
@@ -0,0 +1,10 @@
+// Q4. Area of circle using variables
+#include<iostream>
+using namespace std;
+int main(){
+    const float pi= 3.14;
+    float r=5.0;
+    float area = pi * r * r;
+    cout << "Area of circle: " << area << endl;
+    return 0;
+}
