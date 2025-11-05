d658f2031172c79221b2b6ab02eb2341898afcd5
kajalti@29 <kajaltisirame@gmail.com>
2025-11-05 15:16:31 +0530

    code commit

---
+++ 2_Practice code/20_Ifelse.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    if (a % 2 == 0) {
+        cout << "a is even";
+    } else {
+        cout << "a is odd";
+    }
+    return 0;
+}
