904348760f01bd148712d7336375de9e9cc80f22
kajalti@29 <kajaltisirame@gmail.com>
2025-11-05 16:20:47 +0530

    code commit

---
+++ 2_Practice code/21_Findlargestoftwono.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    int b = 20;
+    if(a > b){
+        cout << "Largest is a: " << a << endl;
+    } else {
+        cout << "Largest is b: " << b << endl;
+    }
+return 0;
+}
