7b08b931144a44e4d4bba4e5604d224385881a7b
kajalti@29 <kajaltisirame@gmail.com>
2025-11-05 11:57:36 +0530

    code commit

---
+++ 1_C++/2_Practice Code/1_basic code/2.second.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"I am a clg student"<<endl;
+    cout<<"I am learning C++"<<endl;
+    cout<<"I am enjoying coding"<<endl;
+    cout<<"I am excited to learn more"<<endl;
+    cout<<"I am looking forward to building projects"<<endl;
+    cout<<"I am ready to face challenges"<<endl;
+    cout<<"I am committed to improving my skills"<<endl;
+    cout<<"I am passionate about programming"<<endl;
+    cout<<"I am eager to collaborate with others"<<endl;
+    cout<<"I am proud to be a part of this community"<<endl;
+    cout<<"I am thankful for the support I receive"<<endl;
+    cout<<"I am motivated to succeed"<<endl;
+    cout<<"I am determined to achieve my goals"<<endl;
+    cout<<"I am inspired by my peers"<<endl;
+    cout<<"I am grateful for the opportunities I have"<<endl;
+    cout<<"I am excited about the future"<<endl;
+    cout<<"I am ready to make a difference"<<endl;
+    cout<<"I am confident in my abilities"<<endl;
+    cout<<"I am always learning and growing"<<endl;
+    cout<<"I am here to make the most of my education"<<endl;
+    cout<<"I am looking forward to new experiences"<<endl;
+    cout<<"I am committed to lifelong learning"<<endl;
+    cout<<"I am proud of my achievements"<<endl;
+    cout<<"I am excited to share my knowledge"<<endl;
+    cout<<"I am ready to take on new challenges"<<endl;
+    cout<<"I am enthusiastic about coding"<<endl;
+    cout<<"I am eager to explore new technologies"<<endl;
+    cout<<"I am passionate about problem-solving"<<endl;
+    cout<<"I am excited to contribute to open source"<<endl;
+    cout<<"I am looking forward to networking with others"<<endl;
+    cout<<"I am ready to embrace change"<<endl;
+    cout<<"I am committed to making a positive impact"<<endl;
+    cout<<"I am excited to be a part of this journey"<<endl;
+    cout<<"I am ready to learn from my mistakes"<<endl;
+return 0;
+}
